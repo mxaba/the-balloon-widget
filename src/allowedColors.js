6c6e94226c5a18dd03204420e999ae20dead9d65
mxaba <samuel.mcebo@gmail.com>
2021-11-08 10:00:52 +0200

    Updates

--- src/allowedColors.js
+++
@@ -1,1 +0,0 @@
-export const allowedColors = ['Aqua', 'Black', 'Blue', 'Fuchsia', 'Gray', 'Green', 'Lime', 'Maroon', 'Navy', 'Olive', 'Orange', 'Purple', 'Red', 'Silver', 'Teal', 'White', 'Yellow'];
